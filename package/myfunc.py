63719d6c6dc86126f32e879a88f2fcaf998096bf
akira1744 <akr1744@gmail.com>
2022-01-18 19:52:47 +0900

    13th

--- package/myfunc.py
+++ package/myfunc.py
@@ -154,7 +154,7 @@ def set_location(select_hpname, hp, pref_list):
 
 
 @st.experimental_memo()
-def filtering_data(hp, select_hpname, mdc2d, mdc6d, oped, set_min, set_max):
+def filtering_data(hp, select_hpname, mdc2d, mdc6d, oped, set_min, set_max,select_prefs,select_med2s,select_citys):
     hp = hp.loc[hp['bed'].between(set_min, set_max)]
     f = set(hp['hpname'])
     f = f.union(select_hpname)
@@ -172,9 +172,9 @@ def filtering_data(hp, select_hpname, mdc2d, mdc6d, oped, set_min, set_max):
 def draw_chart(select_hpname,  mdc2d, mdc6d, oped, hp):
     ##################################################################
     top_hight = 370
-    top_width = 480
+    top_width = 490
     second_hight = 370
-    second_width = 210
+    second_width = 190
     color_scheme = 'category20b'
     #########################################################
     # 医療機関名が選択されたらその病院を赤くする。
@@ -195,7 +195,7 @@ def draw_chart(select_hpname,  mdc2d, mdc6d, oped, hp):
         fields=['mdcname'], empty='all')
     mdc_color = alt.condition(mdc_selection,
                               alt.Color('mdcname:N', title='MDC2',
-                                        legend=None,
+                                        # legend=None,
                                         scale=alt.Scale(scheme=color_scheme)),
                               alt.value('lightgray'))
 
@@ -205,7 +205,6 @@ def draw_chart(select_hpname,  mdc2d, mdc6d, oped, hp):
                                alt.Color('mdcname:N', title='MDC2',
                                          scale=alt.Scale(scheme=color_scheme)),
                                alt.value('lightgray'),
-
                                )
     ope_selection = alt.selection_single(
         fields=['opename'], empty='all')
@@ -215,25 +214,16 @@ def draw_chart(select_hpname,  mdc2d, mdc6d, oped, hp):
                               alt.value('lightgray')
                               )
     ######################################################################
-    hp_bars = alt.Chart(oped).mark_bar().encode(
-        x=alt.X('sum(value)', title='件数'),
-        y=alt.Y('hpname', sort='-x', title=None,
-                axis=alt.Axis(labelColor=label_color)),
-        color=mdc_color,
-        tooltip=['hpname', 'bed:Q', 'mdcname', 'sum(value)']
-    ).properties(
-        width=top_width,
-        height=top_hight,
-        title='病院別実績'
-    ).add_selection(
-        mdc_selection
-    ).transform_filter(
+    oped_base1 = alt.Chart(oped).transform_filter(
         mdc_selection
     ).transform_filter(
         mdc6_selection
-    ).transform_filter(
+    )
+    oped_base2 = oped_base1.transform_filter(
         ope_selection
-    ).transform_joinaggregate(
+    )
+    #######################################################################
+    oped_base3 = oped_base2.transform_joinaggregate(
         hp_value='sum(value)',
         groupby=['hpname']
     ).transform_window(
@@ -242,98 +232,155 @@ def draw_chart(select_hpname,  mdc2d, mdc6d, oped, hp):
     ).transform_filter(
         (alt.datum.hp_rank < 20) & (alt.datum.value > 0)
     )
-    ###################################################################
-    hp_point = alt.Chart(oped).transform_filter(
+
+    hp_bars = oped_base3.encode(
+        x=alt.X('sum(value)', title='件数'),
+        y=alt.Y('hpname', sort='-x', title=None,
+                axis=alt.Axis(labelColor=label_color)),
+        color=mdc_color,
+        tooltip=[alt.Tooltip('hpname',title='病院名'),
+                 alt.Tooltip('bed:Q',title='病床数',format=","),
+                 alt.Tooltip('mdcname',title='MDC2'),
+                 alt.Tooltip('sum(value)',title='件数',format=",")]
+    ).properties(
+        width=top_width,
+        height=top_hight,
+        title='病院別・疾患別実績'
+    )
+
+    hp_text = oped_base3.encode(
+        x=alt.X('sum(value)', title=None),
+        y=alt.Y('hpname', sort='-x', title=None),
+        text=alt.Text('sum(value)',format=',')
+    )
+
+    hp_bars = hp_bars.mark_bar(
+    ) + hp_text.mark_text(
+        align='left',baseline='middle',dx=3
+    ). add_selection(
         mdc_selection
-    ).transform_filter(
-        mdc6_selection
-    ).transform_filter(
-        ope_selection
-    ).transform_aggregate(
+    )
+    ###################################################################
+    hp_point = oped_base2.transform_aggregate(
         sum_value='sum(value)',
         groupby=['hpname', 'bed', 'mdcname', 'hp']
     ).mark_point().encode(
         x=alt.X('sum_value:Q', title='件数'),
         y=alt.Y('bed:Q', title='病床数', scale=alt.Scale(zero=False)),
         color=mdc_color,
-        shape=alt.Shape('hp', title=None),
-        # size=alt.Size('sum_value:Q'),
-        tooltip=['hpname', 'mdcname', 'sum_value:Q']
+        shape=alt.Shape('hp', title='shape'),
+        size=alt.Size('sum_value:Q',title='size'),
+        tooltip=[alt.Tooltip('hpname',title='病院名'),
+                 alt.Tooltip('bed:Q',title='病床数',format=","),
+                 alt.Tooltip('sum_value:Q',title='件数',format=",")]
     ).properties(
         width=top_width,
         height=top_hight,
-        title='病床数実績'
+        title='病床数別・疾患別実績'
     ).add_selection(
         mdc_selection
     )
+
     ###################################################################
-    mdc_bars = alt.Chart(mdc2d).mark_bar().encode(
+    mdc_bars = alt.Chart(mdc2d).encode(
         x=alt.X('sum(value):Q', title=None),
         y=alt.Y('mdcname', sort='-x', title=None),
         color=mdc_color,
-        tooltip=['mdcname', 'sum(value):Q']
-    ).add_selection(
-        mdc_selection
+        tooltip=[alt.Tooltip('mdcname',title='MDC2'),
+        alt.Tooltip('sum(value):Q',title='件数',format=",")]
     ).properties(
         width=second_width,
         height=second_hight,
-        title='MDC2別'
+        title={
+            "text": ['指定地域内患者数　　　　　　　　　MDC2別'],
+            "fontSize": 14,
+            "anchor": "start"}
+    )    
+    mdc_text = alt.Chart(mdc2d).encode(
+        x=alt.X('sum(value)', title=None),
+        y=alt.Y('mdcname', sort='-x', title=None),
+        text=alt.Text('sum(value)',format=',')
     )
-    ###################################################################
-    mdc6_bars = alt.Chart(mdc6d).transform_filter(
+
+    mdc_bars = mdc_bars.mark_bar(
+    ) + mdc_text.mark_text(
+        align='left',baseline='middle',dx=3
+    ). add_selection(
         mdc_selection
-    ).transform_aggregate(
+    )
+
+    ###################################################################
+    mdc6_base = alt.Chart(mdc6d).transform_filter(
+        mdc_selection
+    ).transform_joinaggregate(
         mdc6_value='sum(value):Q',
         groupby=['mdcname', 'mdc6name']
     ).transform_window(
-        mdc6_rank='rank(mdc6_value:Q)',
+        mdc6_rank='dense_rank(mdc6_value:Q)',
         sort=[alt.SortField('mdc6_value', order='descending')]
     ).transform_filter(
         alt.datum.mdc6_rank < 20
-    ).mark_bar(
-    ).encode(
-        x=alt.X('mdc6_value:Q', title=None),
+    )
+    mdc6_bars = mdc6_base.encode(
+        x=alt.X('sum(value):Q', title=None),
         y=alt.Y('mdc6name', sort='-x', title=None),
         color=mdc6_color,
-        tooltip=['mdcname', 'mdc6name', 'mdc6_value:Q']
-    ).add_selection(
-        mdc6_selection
+        tooltip=[alt.Tooltip('mdc6name',title='MDC6'),
+        alt.Tooltip('sum(value):Q',title='件数',format=",")]
     ).properties(
         width=second_width,
         height=second_hight,
         title='MDC6別'
     )
-    ###################################################################
-    ope_bars = alt.Chart(oped).transform_filter(
-        mdc_selection
-    ).transform_filter(
+    mdc6_text = mdc6_base.encode(
+        x=alt.X('sum(value):Q', title=None),
+        y=alt.Y('mdc6name', sort='-x', title=None),
+        text=alt.Text('sum(value):Q',format=',')
+    )
+    mdc6_bars = mdc6_bars.mark_bar(
+    ) + mdc6_text.mark_text(
+        align='left',baseline='middle',dx=3
+    ). add_selection(
         mdc6_selection
-    ).transform_aggregate(
+    )
+    ###################################################################
+    ope_base4 = oped_base1.transform_joinaggregate(
         ope_value='sum(value):Q',
         groupby=['mdcname', 'mdc6name', 'opename']
     ).transform_window(
-        ope_rank='rank(ope_value:Q)',
+        ope_rank='dense_rank(ope_value:Q)',
         sort=[alt.SortField('ope_value', order='descending')]
     ).transform_filter(
         alt.datum.ope_rank < 20
-    ).mark_bar(
-    ).encode(
-        x=alt.X('ope_value:Q', title=None),
+    )
+    ope_bars = ope_base4.encode(
+        x=alt.X('sum(value):Q', title=None),
         y=alt.Y('opename', sort='-x', title=None),
         color=ope_color,
-        tooltip=['mdcname', 'mdc6name', 'opename', 'ope_value:Q']
-    ).add_selection(
-        ope_selection
+        tooltip=[alt.Tooltip('mdcname',title='MDC2'),
+        alt.Tooltip('mdc6name',title='MDC6'),
+        alt.Tooltip('opename',title='手術'),
+        alt.Tooltip('sum(value):Q',title='件数',format=",")]
     ).properties(
         width=second_width,
         height=second_hight,
         title='手術別'
     )
+    ope_text = ope_base4.encode(
+        x=alt.X('sum(value):Q', title=None),
+        y=alt.Y('opename', sort='-x', title=None),
+        text=alt.Text('sum(value):Q',format=',')
+    )
+    ope_bars = ope_bars.mark_bar(
+    ) + ope_text.mark_text(
+        align='left',baseline='middle',dx=3
+    ). add_selection(
+        ope_selection
+    )
     ###################################################################
     # chartの結合　select_hpnameがなければこのchartsを返す
     top_chart = alt.hconcat(hp_bars, hp_point)
     second_chart = alt.hconcat(mdc_bars, mdc6_bars, ope_bars)
-    charts = alt.vconcat(top_chart, second_chart)
     ###################################################################
     # select_hpがあった場合は1病院ずつchartを作成して、chartsの下側に追加する。
     if select_hpname != []:
@@ -355,13 +402,12 @@ def draw_chart(select_hpname,  mdc2d, mdc6d, oped, hp):
             hp_ope = hp_ope.loc[hp_ope['value'] > 0]
             ####################################################
             hp_mdc2_bars = alt.Chart(hp_mdc2).mark_bar().encode(
-                x=alt.X('value:Q', title=None,
+                x=alt.X('value', title=None,
                         scale={'domain': [0, mdc2_max]}),
                 y=alt.Y('mdcname', sort='-x', title=None),
                 color=mdc_color,
-                tooltip=['mdcname', 'value:Q']
-            ).add_selection(
-                mdc_selection
+                tooltip=[alt.Tooltip('mdcname',title='MDC2'),
+                        alt.Tooltip('value:Q',title='件数',format=",")]
             ).properties(
                 width=second_width,
                 height=second_hight,
@@ -371,28 +417,49 @@ def draw_chart(select_hpname,  mdc2d, mdc6d, oped, hp):
                     "fontSize": 12,
                     "anchor": "start"}
             )
-            ####################################################
-            hp_mdc6_bars = alt.Chart(hp_mdc6).transform_filter(
+            hp_mdc2_text = alt.Chart(hp_mdc2).encode(
+                x=alt.X('value', title=None),
+                y=alt.Y('mdcname', sort='-x', title=None),
+                text=alt.Text('value',format=',')
+            )
+            hp_mdc2_bars = hp_mdc2_bars.mark_bar(
+            ) + hp_mdc2_text.mark_text(
+                align='left',baseline='middle',dx=3
+            ). add_selection(
                 mdc_selection
-            ).transform_window(   # 1病院だけなのでtransform_aggregate不要
-                rank='rank(row_number)',
-                sort=[alt.SortField('value', order='descending')]
+            )
+            ####################################################
+            hp_mdc6_base = alt.Chart(hp_mdc6).transform_filter(
+                mdc_selection
+            ).transform_window( 
+                sort=[alt.SortField('value', order='descending')],
+                rank='rank(row_number)'
             ).transform_filter(
                 alt.datum.rank < 20
-            ).mark_bar().encode(
-                x=alt.X('value', title=None,
-                        scale={'domain': [0, mdc6_max]}),
+            )
+            hp_mdc6_bars =hp_mdc6_base.encode(
+                x=alt.X('sum(value)', title=None, scale={'domain': [0, mdc6_max]}),
                 y=alt.Y('mdc6name', sort='-x', title=None),
                 color=mdc6_color,
-                tooltip=['mdc6name', 'value:Q']
-            ).add_selection(
-                mdc6_selection
+                tooltip=[alt.Tooltip('mdc6name',title='MDC6'),
+                        alt.Tooltip('sum(value):Q',title='件数',format=",")]
             ).properties(
                 width=second_width,
                 height=second_hight,
             )
+            hp_mdc6_text = hp_mdc6_base.encode(
+                x=alt.X('sum(value)', title=None, scale={'domain': [0, mdc6_max]}),
+                y=alt.Y('mdc6name', sort='-x', title=None),
+                text=alt.Text('sum(value)',format=',')
+            )
+            hp_mdc6_bars = hp_mdc6_bars.mark_bar(
+            ) + hp_mdc6_text.mark_text(
+                align='left',baseline='middle',dx=3
+            ). add_selection(
+                mdc6_selection
+            )
             ###################################################
-            hp_ope_bars = alt.Chart(hp_ope).transform_filter(
+            hp_ope_base = alt.Chart(hp_ope).transform_filter(
                 mdc_selection
             ).transform_filter(
                 mdc6_selection
@@ -401,22 +468,35 @@ def draw_chart(select_hpname,  mdc2d, mdc6d, oped, hp):
                 sort=[alt.SortField('value', order='descending')]
             ).transform_filter(
                 alt.datum.rank < 20
-            ).mark_bar().encode(
-                x=alt.X('value:Q', title=None,
-                        scale={'domain': [0, ope_max]}),
+            )
+            hp_ope_bars = hp_ope_base.encode(
+                x=alt.X('sum(value):Q', title=None,scale={'domain': [0, ope_max]}),
                 y=alt.Y('opename', sort='-x', title=None),
                 color=ope_color,
-                tooltip=['opename', 'value:Q']
-            ).add_selection(
-                ope_selection
+                        tooltip=[alt.Tooltip('mdcname',title='MDC2'),
+                                alt.Tooltip('mdc6name',title='MDC6'),
+                                alt.Tooltip('opename',title='手術'),
+                                alt.Tooltip('sum(value):Q',title='件数',format=",")]
             ).properties(
                 width=second_width,
                 height=second_hight
             )
+            hp_ope_text = hp_ope_base.encode(
+                x=alt.X('sum(value):Q', title=None, scale={'domain': [0, mdc6_max]}),
+                y=alt.Y('opename', sort='-x', title=None),
+                text=alt.Text('sum(value):Q',format=',')
+            )
+            hp_ope_bars = hp_ope_bars.mark_bar(
+            ) + hp_ope_text.mark_text(
+                align='left',baseline='middle',dx=3
+            ). add_selection(
+                ope_selection
+            )
             ####################################################
             hp_charts = alt.hconcat(hp_mdc2_bars | hp_mdc6_bars | hp_ope_bars)
-            charts = alt.vconcat(charts, hp_charts)
+            top_chart = alt.vconcat(top_chart, hp_charts)
             #####################################################
+    charts = alt.vconcat(top_chart, second_chart)
     charts = charts.configure_title(
         fontSize=14,
         anchor='middle',
